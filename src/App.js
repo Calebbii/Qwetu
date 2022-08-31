3b563325543890317f92d7af287eaecfd0566bf3
Calebbii <biicaleb316@gmail.com>
2022-08-31 17:35:56 +0300

    Updates app.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Axios from "axios"
+import { useState } from 'react';
+import Category from './components/Category';
 function App() {
+
+  const [query, setquery] = useState("")
+  const [meals, setmenu] =  useState([])
+
+  var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
+
+  async function getMenu() {
+    var result = await Axios.get(url);
+    setmenu(result.data.mealsp);
+    console.log(result.data);
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getMenu();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 >Qwetu Dishes</h1>
+      <form className='search' onSubmit={onSubmit}>
+        <input
+        type="text"
+        placeholder='Enter Meal'
+        value={query}
+        onChange={(e) => setquery(e.target.value)}
+        />
+
+        <input type="submit" value="Search"/>
+
+      </form>
+      <div>
+        {meals.map(meals => {
+          return <Category meals={meals}/>
+        })}
+      </div>
     </div>
   );
 }
