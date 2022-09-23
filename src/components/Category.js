d005885559f9049d7fa649f246448ce99b0fe7f1
Calebbii <biicaleb316@gmail.com>
2022-09-23 13:38:38 +0300

    Updates the category section

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import '../style/category.css';
 
 export default function Category({meals}) {
   return (
-    <div className='category'>
-        <img className="categoryImg" src={meals["meals"]["strMealThumb"]}/>
-        <p className='categoryName'>{meals["meals"]["srtCategory"]}</p>
+    <div id='container'>
+      <div className='category' >
+        <img className="categoryImg" src={meals.strMealThumb}/>
+        <h1 className='categoryName'>{meals.strMeal}</h1>
+      </div>
     </div>
+    
   )
 }
