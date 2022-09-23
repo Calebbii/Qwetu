e11d7519bf6e6a41b89d4534c1033c646f0e8ede
Calebbii <biicaleb316@gmail.com>
2022-09-23 13:38:16 +0300

    Updates the display section

--- src/App.js
+++ src/App.js
@@ -1,48 +1,58 @@
 import './App.css';
-import Axios from "axios"
-import { useState } from 'react';
-import Category from './components/Category';
-function App() {
+// import Axios from "axios"
+// import { useState } from 'react';
+// import Category from './components/Category';
 
-  const [query, setquery] = useState("")
-  const [meals, setmenu] =  useState([])
+// function App() {
 
-  var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
+// //   const [query, setquery] = useState("")
+// //   const [meals, setmenu] =  useState([])
 
-  async function getMenu() {
-    var result = await Axios.get(url);
-    setmenu(result.data.mealsp);
-    console.log(result.data);
-  }
+// //   var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    getMenu();
-  }
+// //   async function getMenu() {
+// //     var result = await Axios.get(url);
+// //     setmenu(result.data.meals);
+// //     console.log(result.data.meals);
+// //   }
 
+// //   const onSubmit = (e) => {
+// //     e.preventDefault();
+// //     getMenu();
+// //   }
+
+// //   return (
+// //     <div className="App" style={{height: 10}}>
+// //       <h1 >Qwetu Dishes</h1>
+// //       <form className='search' onSubmit={onSubmit}>
+// //         <input
+// //         type="text"
+// //         placeholder='Enter Meal'
+// //         value={query}
+// //         onChange={(e) => setquery(e.target.value)}
+// //         />
+
+// //         <input type="submit" value="Search"/>
+
+// //       </form>
+// //       <div>
+// //         {meals.map(meals => {
+// //           return <Category meals={meals}key={meals.idMeal}/>
+// //         })}
+// //       </div>
+// //     </div>
+// //   );
+// // }
+
+// // export default App;
+
+import React from 'react'
+import Home from './components/Home'
+
+export default function App() {
   return (
-    <div className="App">
-      <h1 >Qwetu Dishes</h1>
-      <form className='search' onSubmit={onSubmit}>
-        <input
-        type="text"
-        placeholder='Enter Meal'
-        value={query}
-        onChange={(e) => setquery(e.target.value)}
-        />
-
-        <input type="submit" value="Search"/>
-
-      </form>
-      <div>
-        {meals.map(meals => {
-          return <Category meals={meals}/>
-        })}
-      </div>
+    <div className='home'>
+      < Home />
     </div>
-  );
+  )
 }
-
-
-
-export default App;
